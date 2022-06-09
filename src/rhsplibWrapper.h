a24a554f4529c4cc109d7d2be860f7b22e0ed81f
jfabellera <jfabellera@gmail.com>
2022-06-09 12:46:14 -0500

    AsyncWorker-ify existing rhsplib wrapper functions

--- src/rhsplibWrapper.h
+++ src/rhsplibWrapper.h
@@ -10,27 +10,27 @@ class RHSPlib : public Napi::ObjectWrap<RHSPlib> {
 
   RHSPlib(const Napi::CallbackInfo &info);
 
-  void open(const Napi::CallbackInfo &info);
+  Napi::Value open(const Napi::CallbackInfo &info);
   Napi::Value isOpened(const Napi::CallbackInfo &info);
   void close(const Napi::CallbackInfo &info);
   void setDestAddress(const Napi::CallbackInfo &info);
   Napi::Value getDestAddress(const Napi::CallbackInfo &info);
   void setResponseTimeoutMs(const Napi::CallbackInfo &info);
   Napi::Value getResponseTimeoutMs(const Napi::CallbackInfo &info);
-  void sendWriteCommandInternal(const Napi::CallbackInfo &info);
+  Napi::Value sendWriteCommandInternal(const Napi::CallbackInfo &info);
   Napi::Value sendWriteCommand(const Napi::CallbackInfo &info);
-  void sendReadCommandInternal(const Napi::CallbackInfo &info);
+  Napi::Value sendReadCommandInternal(const Napi::CallbackInfo &info);
   Napi::Value sendReadCommand(const Napi::CallbackInfo &info);
   Napi::Value getModuleStatus(const Napi::CallbackInfo &info);
-  void sendKeepAlive(const Napi::CallbackInfo &info);
-  void sendFailSafe(const Napi::CallbackInfo &info);
-  void setNewModuleAddress(const Napi::CallbackInfo &info);
+  Napi::Value sendKeepAlive(const Napi::CallbackInfo &info);
+  Napi::Value sendFailSafe(const Napi::CallbackInfo &info);
+  Napi::Value setNewModuleAddress(const Napi::CallbackInfo &info);
   Napi::Value queryInterface(const Napi::CallbackInfo &info);
-  void setModuleLEDColor(const Napi::CallbackInfo &info);
+  Napi::Value setModuleLEDColor(const Napi::CallbackInfo &info);
   Napi::Value getModuleLEDColor(const Napi::CallbackInfo &info);
-  void setModuleLEDPattern(const Napi::CallbackInfo &info);
+  Napi::Value setModuleLEDPattern(const Napi::CallbackInfo &info);
   Napi::Value getModuleLEDPattern(const Napi::CallbackInfo &info);
-  void setDebugLogLevel(const Napi::CallbackInfo &info);
+  Napi::Value setDebugLogLevel(const Napi::CallbackInfo &info);
   static Napi::Value discovery(const Napi::CallbackInfo &info);
   Napi::Value getInterfacePacketID(const Napi::CallbackInfo &info);
 
