01e7d7cee50ecde9e879270cd1808661d3368d48
jfabellera <jfabellera@gmail.com>
2022-06-16 14:47:05 -0500

    Add serial port wrapper

---
+++ src/serialWrapper.h
@@ -0,0 +1,22 @@
+#ifndef SERIAL_WRAPPER_H_
+#define SERIAL_WRAPPER_H_
+
+#include <RHSPlib_serial.h>
+#include <napi.h>
+
+class Serial : public Napi::ObjectWrap<Serial> {
+ public:
+  static Napi::Object Init(Napi::Env env, Napi::Object exports);
+
+  Serial(const Napi::CallbackInfo &info);
+
+  void open(const Napi::CallbackInfo &info);
+  void close(const Napi::CallbackInfo &info);
+  Napi::Value read(const Napi::CallbackInfo &info);
+  void write(const Napi::CallbackInfo &info);
+
+ private:
+  RHSPlib_Serial_T serialPort;
+};
+
+#endif
