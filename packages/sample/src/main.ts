5c6c01fb8a8195745bae010ee4781276b47542fd
LandryNorris <landry.norris@live.com>
2023-05-04 09:59:16 -0500

    Add motor control to sample

--- packages/sample/src/main.ts
+++ packages/sample/src/main.ts
@@ -5,7 +5,9 @@ import {RevHub} from "@rev-robotics/expansion-hub";
 const program = new Command();
 
 program.version('1.0.0')
-    .option('-l --list', 'List connected devices').parse(process.argv);
+    .option('-l --list', 'List connected devices')
+    .option('-m --motor <power>', 'Set motor power')
+    .parse(process.argv);
 
 const options = program.opts();
 
@@ -17,6 +19,18 @@ if(options.list) {
     hubs.forEach(async (hub) => {
         console.log(await toString(hub));
     });
+}
+
+if(options.motor) {
+    let power: number = Number(options.motor);
+
+    const hubs: RevHub[] = await getConnectedExpansionHubs();
+    let hub = hubs[0]
+
+    await hub.setMotorChannelMode(1, 0, true);
+    console.log(`Power is ${power}, type is ${typeof power}`)
+    await hub.setMotorConstantPower(1, power);
+    await hub.setMotorChannelEnable(1, true);
 }
 
 async function toString(hub: RevHub): Promise<string> {
