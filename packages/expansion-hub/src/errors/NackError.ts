1c65976e828d4149d42cc7da98ada5a18537dd1e
LandryNorris <landry.norris@live.com>
2023-05-08 15:19:02 -0500

    Convert to Object.setPrototype, which is less deprecated

--- packages/expansion-hub/src/errors/NackError.ts
+++ packages/expansion-hub/src/errors/NackError.ts
@@ -10,10 +10,15 @@ in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
+---
+
+Modified to use Object.setPrototypeOf instead of __proto__
+
  */
 export const {
     setPrototypeOf = function (obj: any, proto: any) {
-        obj.__proto__ = proto;
+        Object.setPrototypeOf(obj, proto);
         return obj;
     },
 } = Object;
