ac414d0c4a78de5cc0f533ed46ac8ddbe1302a31
LandryNorris <landry.norris@live.com>
2023-05-08 09:23:35 -0500

    Add error flag to sample

--- packages/sample/src/main.ts
+++ packages/sample/src/main.ts
@@ -1,11 +1,13 @@
 import {Command} from "commander";
-import {getConnectedExpansionHubs} from "@rev-robotics/expansion-hub";
+import {getConnectedExpansionHubs, openParentRevHub} from "@rev-robotics/expansion-hub";
 import {RevHub} from "@rev-robotics/expansion-hub";
 
 const program = new Command();
 
 program.version('1.0.0')
-    .option('-l --list', 'List connected devices').parse(process.argv);
+    .option('-l --list', 'List connected devices')
+    .option('-e --error', 'error out')
+    .parse(process.argv);
 
 const options = program.opts();
 
@@ -17,6 +19,16 @@ if(options.list) {
     hubs.forEach(async (hub) => {
         console.log(await toString(hub));
     });
+}
+
+if(options.error) {
+    try {
+        await openParentRevHub("a serial number that isn't correct")
+        console.log("Expected error, but got none")
+    } catch(e) {
+        console.log("Correctly got error.")
+        console.log(`Error is:\n\t${e}`)
+    }
 }
 
 async function toString(hub: RevHub): Promise<string> {
