e5ef978bf614b26a5281b77d5b4b513cb684b682
LandryNorris <landry.norris@live.com>
2023-05-08 15:12:22 -0500

    Add proper attribution

--- packages/expansion-hub/src/errors/NackError.ts
+++ packages/expansion-hub/src/errors/NackError.ts
@@ -1,5 +1,16 @@
 import {RevHubError} from "./RevHubError";
 
+//Taken from https://github.com/apollographql/invariant-packages/blob/779f525d6b475653050cb17976753de287e30f91/packages/ts-invariant/src/invariant.ts#L2-L7
+/*
+Copyright (c) 2019 Apollo GraphQL
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+ */
 export const {
     setPrototypeOf = function (obj: any, proto: any) {
         obj.__proto__ = proto;
