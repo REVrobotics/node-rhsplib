14a6460108034db39d5f241c8af967b638cc00b1
LandryNorris <landry.norris@live.com>
2023-05-11 12:39:39 -0500

    Change name to intervalMs

--- packages/expansion-hub/src/start-keep-alive.ts
+++ packages/expansion-hub/src/start-keep-alive.ts
@@ -3,9 +3,9 @@ import {ExpansionHubInternal} from "./internal/ExpansionHub";
 /**
  * Starts a keep alive task for a given hub.
  * @param hub hub to start the keep alive for
- * @param interval interval in ms between each keep alive
+ * @param intervalMs interval in ms between each keep alive
  */
-export function startKeepAlive(hub: ExpansionHubInternal, interval: number) {
+export function startKeepAlive(hub: ExpansionHubInternal, intervalMs: number) {
     if(hub.keepAliveTimer) {
         //stop existing keep alive timer, so we can start a new one.
         clearInterval(hub.keepAliveTimer);
@@ -14,5 +14,5 @@ export function startKeepAlive(hub: ExpansionHubInternal, interval: number) {
         hub.sendKeepAlive().catch((e: any) => {
             hub.emitError(e);
         });
-    }, interval);
+    }, intervalMs);
 }
