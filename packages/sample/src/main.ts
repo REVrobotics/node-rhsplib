10eaf12a69abce759e5616e9b9ae4dd111dbc81c
LandryNorris <landry.norris@live.com>
2023-05-18 16:15:12 -0500

    Add button press demo

--- packages/sample/src/main.ts
+++ packages/sample/src/main.ts
@@ -1,11 +1,13 @@
 import {Command} from "commander";
 import {ExpansionHub, getConnectedExpansionHubs} from "@rev-robotics/expansion-hub";
 import {RevHub} from "@rev-robotics/expansion-hub";
+import { DIODirection } from "@rev-robotics/rhsplib";
 
 const program = new Command();
 
 program.version('1.0.0')
     .option('-l --list', 'List connected devices')
+    .option('-b --button', 'Run button press demo')
     .parse(process.argv);
 
 const options = program.opts();
@@ -28,6 +30,30 @@ if(options.list) {
             hub.close();
         })
     }, 2000);
+}
+
+if (options.button) {
+    let hubs = await getConnectedExpansionHubs();
+    console.log("Press a button:");
+
+    for (let i = 0; i < 8; i++) {
+        await hubs[0].setDigitalDirection(i, DIODirection.Input);
+    }
+
+    let lastStates = 0;
+
+    while (true) {
+        for (let i = 0; i < 8; i++) {
+            let state = await hubs[0].getDigitalSingleInput(i);
+            let lastState = ((lastStates >> i) & 0x1) != 0;
+            if (!state && lastState) {
+                console.log(`Pressed button for input ${i}`);
+            }
+
+            let newBit = state ? 1 : 0;
+            lastStates = (lastStates & ~(1 << i)) | (newBit << i);
+        }
+    }
 }
 
 async function toString(hub: RevHub): Promise<string> {
