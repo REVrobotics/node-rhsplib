64b740af308bfda4df57607e197cc36a4c76e254
LandryNorris <landry.norris@live.com>
2023-05-24 15:30:57 -0500

    Make High and Low readonly

--- packages/core/src/digital-state.ts
+++ packages/core/src/digital-state.ts
@@ -1,6 +1,6 @@
 export class DigitalState {
-    static High = new DigitalState(true);
-    static Low = new DigitalState(false);
+    static readonly High = new DigitalState(true);
+    static readonly Low = new DigitalState(false);
 
     private readonly state: boolean;
     private constructor(state: boolean) {
