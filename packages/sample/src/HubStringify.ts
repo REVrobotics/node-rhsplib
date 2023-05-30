41ae6a35732081f014d88772520778e6bf399f33
LandryNorris <landry.norris@live.com>
2023-05-30 09:50:45 -0500

    Show serial number for parent expansion hub

--- packages/sample/src/HubStringify.ts
+++ packages/sample/src/HubStringify.ts
@@ -1,13 +1,10 @@
 import { RevHub } from "@rev-robotics/expansion-hub";
 
 export function hubHierarchyToString(hub: RevHub): string {
-    let result = `USB Expansion Hub: ${hub.moduleAddress}\n`;
-
-    if (hub.isExpansionHub()) {
-        console.log(`Is open? ${hub.isOpen}`);
-    }
+    let result = "";
 
     if (hub.isParent()) {
+        result = `USB Expansion Hub: ${hub.serialNumber} ${hub.moduleAddress}\n`;
         for (const child of hub.children) {
             result += `\tUSB Expansion Hub: ${child.moduleAddress}\n`;
         }
