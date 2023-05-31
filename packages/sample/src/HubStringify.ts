4d9de146cf6135919c5e73872722caf528988140
LandryNorris <landry.norris@live.com>
2023-05-31 09:18:33 -0500

    Remove extra logs

--- packages/sample/src/HubStringify.ts
+++ packages/sample/src/HubStringify.ts
@@ -3,12 +3,7 @@ import { RevHub } from "@rev-robotics/expansion-hub";
 export function hubHierarchyToString(hub: RevHub): string {
     let result = `RevHub: ${hub.moduleAddress}\n`;
 
-    if (hub.isExpansionHub()) {
-        console.log(`Is open? ${hub.isOpen}`);
-    }
-
     if (hub.isParent()) {
-        console.log(hub.serialNumber);
         for (const child of hub.children) {
             result += `\tRevHub: ${child.moduleAddress}\n`;
         }
