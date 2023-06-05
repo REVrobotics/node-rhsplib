5df21bfbaec30298310f8c3978973760f1581ff1
LandryNorris <landry.norris@live.com>
2023-06-05 17:03:26 -0500

    Send commands when control hub methods called

--- packages/control-hub/src/index.ts
+++ packages/control-hub/src/index.ts
@@ -1,2 +1,13 @@
+import { ControlHubInternal } from "./internal/ControlHub.js";
+import { ControlHub } from "@rev-robotics/rev-hub-core";
 
 export { openConnectedControlHub } from "./discovery.js";
+
+export async function openControlHub(
+    serialNumber: string,
+    port: number,
+): Promise<ControlHub> {
+    let hub = new ControlHubInternal(serialNumber);
+    await hub.open("127.0.0.1", (port + 1).toString());
+    return hub;
+}
