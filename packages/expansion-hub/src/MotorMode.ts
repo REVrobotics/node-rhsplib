0e729e7b6321b7a8269ae1a4a2856aee379b856c
LandryNorris <landry.norris@live.com>
2023-06-07 09:49:43 -0500

    Rename motor modes to be more clear

--- packages/expansion-hub/src/MotorMode.ts
+++ packages/expansion-hub/src/MotorMode.ts
@@ -1,7 +1,19 @@
 //The order of these should match the RHSPlib documentation. We rely on the
 //numerical value matching.
 export enum MotorMode {
-    CONSTANT_POWER,
-    CONSTANT_VELOCITY,
-    TARGET_POSITION,
+    /**
+     * This mode controls the motor's speed by setting the pwm duty cycle.
+     * It does not require an encoder.
+     */
+    OPEN_LOOP,
+    /**
+     * This mode uses the encoder to regulate the velocity of the motor.
+     * This mode requires an encoder to be connected.
+     */
+    REGULATED_VELOCITY,
+    /**
+     * This mode uses an encoder to regulate the position of the motor.
+     * This mode requires an encoder to be connected.
+     */
+    REGULATED_POSITION,
 }
