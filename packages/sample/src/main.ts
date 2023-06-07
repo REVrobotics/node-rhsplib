538e81e4de9792d1e04a79eb141b68d0de57f411
LandryNorris <landry.norris@live.com>
2023-06-07 10:11:26 -0500

    Add motor position command

--- packages/sample/src/main.ts
+++ packages/sample/src/main.ts
@@ -6,6 +6,7 @@ import {
     runEncoder,
     runMotorConstantPower,
     runMotorConstantVelocity,
+    runMotorToPosition,
 } from "./command/motor.js";
 import { list } from "./command/list.js";
 import { led } from "./command/led.js";
@@ -87,6 +88,16 @@ motorCommand.command("velocity <channel> <speed>").action(async (channel, speed)
     let speedNumber = Number(speed);
     await runMotorConstantVelocity(channelNumber, speedNumber);
 });
+
+motorCommand
+    .command("position <channel> <position> <tolerance>")
+    .action(async (channel, position, tolerance) => {
+        console.log(`${channel} ${position} ${tolerance}`);
+        let channelNumber = Number(channel);
+        let positionNumber = Number(position);
+        let toleranceNumber = Number(tolerance);
+        await runMotorToPosition(channelNumber, positionNumber, toleranceNumber);
+    });
 
 program
     .command("list")
