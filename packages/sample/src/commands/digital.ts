3e6c824591badb283462544981a94f3e6c215736
LandryNorris <landry.norris@live.com>
2023-06-27 10:19:45 -0500

    remove extra hub opening and fix formatting

--- packages/sample/src/commands/digital.ts
+++ packages/sample/src/commands/digital.ts
@@ -1,33 +1,24 @@
-import {
-  ExpansionHub,
-  openConnectedExpansionHubs,
-} from "@rev-robotics/expansion-hub";
+import { ExpansionHub, openConnectedExpansionHubs } from "@rev-robotics/expansion-hub";
 import { DigitalState } from "@rev-robotics/expansion-hub/dist/digital-state.js";
 import { DigitalChannelDirection } from "@rev-robotics/expansion-hub/dist/DigitalChannelDirection.js";
 
 export async function digitalRead(
-  hub: ExpansionHub,
-  channel: number,
-  continuous: boolean
+    hub: ExpansionHub,
+    channel: number,
+    continuous: boolean,
 ): Promise<void> {
-  if (continuous) {
     while (true) {
-      let state = await hub.getDigitalInput(channel);
-      console.log(`${state}`);
+        let state = await hub.getDigitalInput(channel);
+        console.log(`${state}`);
+        if (!continuous) break;
     }
-  } else {
-    let state = await hub.getDigitalInput(channel);
-    console.log(`${state}`);
-  }
 }
 
 export async function digitalWrite(
-  hub: ExpansionHub,
-  channel: number,
-  state: DigitalState
+    hub: ExpansionHub,
+    channel: number,
+    state: DigitalState,
 ): Promise<void> {
-  let hubs = await openConnectedExpansionHubs();
-
-  await hub.setDigitalDirection(channel, DigitalChannelDirection.Output);
-  await hub.setDigitalOutput(channel, state);
+    await hub.setDigitalDirection(channel, DigitalChannelDirection.Output);
+    await hub.setDigitalOutput(channel, state);
 }
