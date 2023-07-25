ad3ef4fb7b983a06025951eadd7d8a445a4020bc
NoahAndrews <noah@revrobotics.com>
2023-07-25 15:17:14 -0500

    Fix openControlHub parameters

--- packages/control-hub/src/index.ts
+++ packages/control-hub/src/index.ts
@@ -2,11 +2,10 @@ import { ControlHubInternal } from "./internal/ControlHub.js";
 import { ControlHub } from "@rev-robotics/rev-hub-core";
 
 export async function openControlHub(
-    serialNumber: string,
-    moduleAddress: number,
+    hostname: string,
     port: number,
 ): Promise<ControlHub> {
-    let hub = new ControlHubInternal(serialNumber);
-    await hub.open("127.0.0.1", port + 1);
+    let hub = new ControlHubInternal("unknown");
+    await hub.open(hostname, port + 1);
     return hub;
 }
