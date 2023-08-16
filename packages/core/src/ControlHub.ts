6c09ca6d1fd2d6ab34c7b695383c0bf098cb3847
LandryNorris <landry.norris@live.com>
2023-08-16 09:24:42 -0500

    Add method overloads to ControlHub

--- packages/core/src/ControlHub.ts
+++ packages/core/src/ControlHub.ts
@@ -1,9 +1,18 @@
-import { ExpansionHub } from "./ExpansionHub.js";
+import { ExpansionHub, ParentExpansionHub } from "./ExpansionHub.js";
 import { ParentRevHub } from "./RevHub.js";
+import { ModuleStatus } from "./ModuleStatus.js";
 
 export interface ControlHub extends ExpansionHub, ParentRevHub {
+    on(eventName: "error", listener: (error: Error) => void): this;
+    on(eventName: "statusChanged", listener: (status: ModuleStatus) => void): this;
+    on(
+        eventName: "addressChanged",
+        listener: (oldAddress: number, newAddress: number) => void,
+    ): this;
+    on(eventName: "sessionEnded", listener: () => void): this;
+
     addUsbConnectedHub(
         serialNumber: string,
         moduleAddress: number,
-    ): Promise<ParentRevHub>;
+    ): Promise<ParentExpansionHub>;
 }
