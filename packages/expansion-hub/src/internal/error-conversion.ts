cd067f1fc0bea0c482e6d443bcae8b96e1759a1c
NoahAndrews <noah@revrobotics.com>
2023-08-25 10:51:55 -0500

    Clean up createError()
    * Fixes warnings
    * Improves performance for NACK errors and non-librhsp errors
      (this doesn't really matter, but 🤷🏻)

--- packages/expansion-hub/src/internal/error-conversion.ts
+++ packages/expansion-hub/src/internal/error-conversion.ts
@@ -26,33 +26,38 @@ export function convertErrorSync<T>(serialNumber: string | undefined, block: () 
 }
 
 function createError(e: any, serialNumber: string | undefined): any {
-    if (e.errorCode == RhspLibErrorCode.GENERAL_ERROR) {
-        return new RhspLibError("General librhsp error");
-    } else if (e.errorCode == RhspLibErrorCode.MSG_NUMBER_MISMATCH) {
-        return new RhspLibError("Message Number Mismatch");
-    } else if (e.errorCode == RhspLibErrorCode.NOT_OPENED) {
-        return new RhspLibError("Hub is not opened");
-    } else if (e.errorCode == RhspLibErrorCode.COMMAND_NOT_SUPPORTED) {
-        return new CommandNotSupportedError();
-    } else if (e.errorCode == RhspLibErrorCode.UNEXPECTED_RESPONSE) {
-        return new RhspLibError("Unexpected packet received");
-    } else if (e.errorCode == RhspLibErrorCode.TIMEOUT) {
-        return new TimeoutError();
-    } else if (e.errorCode == RhspLibErrorCode.NO_HUBS_DISCOVERED) {
-        return new HubNotRespondingError(`The REV Hub with serial number ${serialNumber} did not respond when spoken to. ` +
-                                         `It may be soft-bricked and need its firmware re-installed.`);
-    } else if (e.errorCode == RhspLibErrorCode.SERIAL_ERROR) {
-        return new GeneralSerialError(
-            serialNumber ?? "no serial number provided",
-        );
-    } else if (
-        e.errorCode >= RhspLibErrorCode.ARG_OUT_OF_RANGE_END &&
-        e.errorCode <= RhspLibErrorCode.ARG_OUT_OF_RANGE_START
-    ) {
-        let index = -e.errorCode + RhspLibErrorCode.ARG_OUT_OF_RANGE_START;
-        return new ParameterOutOfRangeError(index);
-    } else if (e.nackCode !== undefined) {
+    // noinspection JSUnresolvedReference
+    const errorCode: number | undefined = e.errorCode;
+
+    if (e.nackCode != undefined) {
         return nackCodeToError(e.nackCode);
+    } else if (errorCode != undefined) {
+        if (errorCode == RhspLibErrorCode.GENERAL_ERROR) {
+            return new RhspLibError("General librhsp error");
+        } else if (errorCode == RhspLibErrorCode.MSG_NUMBER_MISMATCH) {
+            return new RhspLibError("Message Number Mismatch");
+        } else if (errorCode == RhspLibErrorCode.NOT_OPENED) {
+            return new RhspLibError("Hub is not opened");
+        } else if (errorCode == RhspLibErrorCode.COMMAND_NOT_SUPPORTED) {
+            return new CommandNotSupportedError();
+        } else if (errorCode == RhspLibErrorCode.UNEXPECTED_RESPONSE) {
+            return new RhspLibError("Unexpected packet received");
+        } else if (errorCode == RhspLibErrorCode.TIMEOUT) {
+            return new TimeoutError();
+        } else if (errorCode == RhspLibErrorCode.NO_HUBS_DISCOVERED) {
+            return new HubNotRespondingError(`The REV Hub with serial number ${serialNumber} did not respond when spoken to. ` +
+                `It may be soft-bricked and need its firmware re-installed.`);
+        } else if (errorCode == RhspLibErrorCode.SERIAL_ERROR) {
+            return new GeneralSerialError(
+                serialNumber ?? "no serial number provided",
+            );
+        } else if (
+            errorCode >= RhspLibErrorCode.ARG_OUT_OF_RANGE_END &&
+            errorCode <= RhspLibErrorCode.ARG_OUT_OF_RANGE_START
+        ) {
+            let index = -errorCode + RhspLibErrorCode.ARG_OUT_OF_RANGE_START;
+            return new ParameterOutOfRangeError(index);
+        }
     } else {
         return e;
     }
